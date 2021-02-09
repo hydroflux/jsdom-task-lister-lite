282cc56a20b471d801fbfe1cf1881ba78a0c175c
Jack Hubert <jphubert93@gmail.com>
2021-02-09 14:10:49 -0700

    solved

--- src/index.js
+++ src/index.js
@@ -1,3 +1,37 @@
-document.addEventListener("DOMContentLoaded", () => {
-  // your code here
-});
+// document.addEventListener("DOMContentLoaded", () => {
+//   // your code here
+// });
+
+const $createTaskForm = document.getElementById("create-task-form")
+const $taskList = document.getElementById("tasks")
+
+$createTaskForm.addEventListener( "submit" , addTask )
+
+function addTask(event){
+  event.preventDefault()
+  const taskDescription = getTaskDescription(event)
+
+  $taskList.appendChild(createTask(taskDescription))
+}
+
+function getTaskDescription(event){
+  const taskForm = new FormData(event.target)
+  return taskForm.get("new-task-description")
+}
+
+function createTask(taskDescription){
+  $li = document.createElement("li")
+  $li.textContent = taskDescription
+
+  const $delete = document.createElement("button")
+  $delete.textContent = "x"
+
+  $li.appendChild($delete)
+  $delete.addEventListener( "click" , deleteTask )
+
+  return $li
+}
+
+function deleteTask(event){
+  event.path[1].remove()
+}
